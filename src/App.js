d4c60c7c37582548a9c96022a0256ef567a62ce0
SAN <san-mail@ukr.net>
2024-03-24 09:28:57 +0200

    Second changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { bkColor: this.props.bkColor, txtColor: this.props.txtColor, text: this.props.text };
+  }
+  render() {
+    const handlerClick = () => {
+      this.props.onClickAct(this.state.bkColor, this.state.txtColor, this.state.text);
+    };
+    return (
+      <button className="Button" onClick={handlerClick} style={{ backgroundColor: this.state.bkColor }}>
+        <p style={{ color: this.state.txtColor }}>{this.state.text}</p>
+      </button>
+    );
+  }
+}
+class DisplayBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { bkColor: this.props.bkColor, txtColor: this.props.txtColor };
+  }
+  render() {
+    return (
+      <div className="DisplayBlock" style={{ backgroundColor: this.props.bkColor }}>
+          <p style={{ color: this.props.txtColor }}>Some text</p>
+      </div>
+    );
+  }
+}
+export default function App() {
+  const [displayBkColor, setDisplayBkColor] = useState("white");
+  const [displayTxtColor, setDisplayTxtColor] = useState("black");
+  const stateFunc = (newBkColor, newTxtColor) => {
+    setDisplayBkColor(newBkColor);
+    setDisplayTxtColor(newTxtColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button bkColor="red" txtColor="green" text="Red" onClickAct={stateFunc} />
+      <Button bkColor="green" txtColor="red" text="Green" onClickAct={stateFunc} />
+      <Button bkColor="yellow" txtColor="blue" text="Yellow" onClickAct={stateFunc}/>
+      <DisplayBlock bkColor={displayBkColor} txtColor={displayTxtColor}/>
+    </>
   );
 }
-
-export default App;
